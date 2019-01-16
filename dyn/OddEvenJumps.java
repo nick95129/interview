89d80ac5cd76c754545e6d1095d7aaae18160439
Qiaolin Jin <qiaolinjin@qiaolins-mbp.attlocal.net>
2019-01-15 22:49:22 -0800

    add comments

--- dyn/OddEvenJumps.java
+++ dyn/OddEvenJumps.java
@@ -4,6 +4,14 @@ import java.util.TreeMap;
 
 /*
   https://leetcode.com/contest/weekly-contest-119/problems/odd-even-jump/
+Input: [10,13,12,14,15]
+Output: 2
+Explanation: 
+From starting index i = 0, we can jump to i = 2 (since A[2] is the smallest among A[1], A[2], A[3], A[4] that is greater or equal to A[0]), then we can't jump any more.
+From starting index i = 1 and i = 2, we can jump to i = 3, then we can't jump any more.
+From starting index i = 3, we can jump to i = 4, so we've reached the end.
+From starting index i = 4, we've reached the end already.
+In total, there are 2 different starting indexes (i = 3, i = 4) where we can reach the end with some number of jumps.
 */
 
 
